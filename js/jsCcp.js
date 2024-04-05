addd420e4ca2b9ddc34c61a2a1d3ecf81a5bb55a
RaymoneXTY <143548425+RaymoneXTY@users.noreply.github.com>
2024-04-05 11:56:40 -0400

    Add files via upload

--- js/jsCcp.js
+++ js/jsCcp.js
@@ -1,6 +1,7 @@
 document.querySelectorAll("img").forEach(function(img) {
     img.addEventListener("mouseover", function() {
         img.style.transform = "scale(1.2)";
+        img.style.cursor = "pointer";
     });
 
     img.addEventListener("mouseout", function() {
@@ -18,4 +19,9 @@ document.querySelectorAll("video").forEach(function(img) {
     });
 });
 
-
+// open a new page for the image when clicked
+document.querySelectorAll("img").forEach(function(img) {
+    img.addEventListener("click", function() {
+        window.open(img.src);
+    });
+}   );
